6d233df4b5e10e5d4ef953d5d9232b7ef10d2f9f
Kaiser Pister <pisterk@gmail.com>
2018-02-13 14:17:01 -0800

    fixed bugs from testing, added known_hosts

--- README.md
+++ README.md
@@ -8,4 +8,24 @@ queue.json contains a list of shell commands which should be executed. These are
 
 task\_manager.py is annotated well within itself.
 
+## Usage:
+
+Run `pip install paramiko` to setup the environment.
+
+Update clients.json with your available computers.
+
+Update queue.json with the work you want distributed.
+
+Run `python task_manager.py`
+
+## Work to be done:
+
+[ ] - Error handling
+
+[ ] - Allow more config options on clients
+
+[ ] - Allow more config options on jobs
+
+[ ] - More testing
+
 # Work done for Calit2 by Kaiser Pister 2018
--- task_manager.py
+++ task_manager.py
@@ -3,30 +3,6 @@ import paramiko
 import json
 from threading import Thread, Lock
 
-# read config file for clients
-clients_cfg = json.load(open("clients.json"))
-
-# list of ssh clients instead
-clients = build_clients(clients_cfg)
-
-# read task queue file
-queue = json.load(open("queue.json"))
-
-# create a lock for the queue to prevent race conditions
-lock = Lock()
-
-# create a list to store all the threads
-threads = []
-
-# start each client to execute tasks from the queue
-for client in clients:
-    threads.append(Thread(target=run_tasks, args=(client, queue, lock)))
-
-# start all threads, and then wait for them to all finish
-[thread.start() for thread in threads]
-[thread.join() for thread in threads]
-
-
 def run_tasks(client, queue, lock):
     # loop over queue
     while len(queue) > 0:
@@ -36,6 +12,7 @@ def run_tasks(client, queue, lock):
         task = process(task)
 
         stdin,stdout,stderr = client.exec_command(task)
+        print("EXECUTED: ", stdout.readlines())
 
 def process(task):
     # do something to build it from a json object
@@ -45,9 +22,39 @@ def process(task):
 def build_clients(config):
     ssh_clients = [] 
     for client in config:
+        print(client)
         ssh_client = paramiko.SSHClient()
+        ssh_client.load_system_host_keys()
         ssh_client.connect(hostname=client['hostname'], 
                            username=client['username'], 
-                           password=['password'])       # and other things as needed
-        clients.append(ssh_client)
+                           password=client['password'])       # and other things as needed
+        ssh_clients.append(ssh_client)
     return ssh_clients
+
+if __name__ == '__main__':
+    # read config file for clients
+    clients_cfg = json.load(open("clients.json"))
+
+    # list of ssh clients instead
+    clients = build_clients(clients_cfg)
+    print("connected to clients")
+
+    # read task queue file
+    queue = json.load(open("queue.json"))
+    print(queue)
+
+    # create a lock for the queue to prevent race conditions
+    lock = Lock()
+
+    # create a list to store all the threads
+    threads = []
+
+    # start each client to execute tasks from the queue
+    for client in clients:
+        threads.append(Thread(target=run_tasks, args=(client, queue, lock)))
+
+    # start all threads, and then wait for them to all finish
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    print("finished all work")
+
